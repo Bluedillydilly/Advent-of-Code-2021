a7cd05c6320876b9cd0b5113406e98237d36620c
Bluedillydilly <dylancuprewich@gmail.com>
2021-12-21 18:27:17 -0500

    day 10 part 1. Used stack instead of symbol counts

---
+++ day10/day10part1.py
@@ -0,0 +1,33 @@
+from typing import NamedTuple
+
+class Symbol(NamedTuple):
+    start: str
+    end: str
+    score: int
+
+def syntaxErrorScore() -> int:
+    f = open("input", "r")
+    
+    errorScore = 0
+    scores = {')': 3, ']':57, '}':1197, '>':25137}
+    pair = {')': '(', ']':'[', '}':'{', '>':'<'}
+    
+    for line in f:
+        l = line.strip()
+        stack: list = []
+        
+        for char in l:
+            if char in pair.values(): # opening
+                stack.append(char)
+            else: # closing
+                if stack[-1] == pair[char]: # valid closer
+                    stack.pop(-1)
+                else: # misplaced closer
+                    errorScore += scores[char]
+                    break
+    f.close()
+                
+    return errorScore
+    
+if __name__ == "__main__":
+    print( "Total error score: ", syntaxErrorScore())
